15ea3b283cea58c7fe32f4f524e6778ab51e3c69
ganaa0910 <ganerdene.dev@gmail.com>
2026-01-24 17:17:45 +0800

    update

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,11 +1,13 @@
 export interface Project {
-  title: string;
-  description: string;
-  description2: string;
-  image: string;
-  tags: string[];
-  projectImage1?: string;
-  projectImage2?: string;
-  servicesProvided?: string;
-  projectDuration?: string;
+	title: string;
+	description: string;
+	description2: string;
+	image: string;
+	tags: string[];
+	projectImage1?: string;
+	projectImage2?: string;
+	servicesProvided?: string;
+	projectDuration?: string;
 }
+
+// kasndklansldnlkasn
